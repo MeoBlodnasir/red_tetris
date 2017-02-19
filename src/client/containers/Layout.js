8475539328e3330b584cc2d1d4550ae04b3d32c1
gonzo <blodnasir@gmail.com>
2017-02-19 13:58:30 +0100

    working

--- src/client/containers/Layout.js
+++ src/client/containers/Layout.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { connect } from 'react-redux'
 import Button from '../components/Button'
+import { upNum } from '../actions/numActions'
 
 const Layout = ({num, onButtonClick}) => (
 	<Button num={num} onClick={onButtonClick} />
@@ -8,20 +9,14 @@ const Layout = ({num, onButtonClick}) => (
 
 const mapStateToProps = (state) => {
 	return {
-		num: state.num
-	}
-}
-
-const handleClick = () => {
-	return {
-		type: "UP",
+		num: state.num.num
 	}
 }
 
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onButtonClick: () => { dispatch(handleClick()) }
+		onButtonClick: () => { dispatch(upNum()) }
 	}
 }
 
